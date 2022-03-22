c99e6d55e611343dd8a4a945bf2b56dc13818c49
= <lucaskorz10@gmail.com>
2022-03-22 13:03:39 -0300

    Alterações concluídas

--- src/components/select/select.js
+++ src/components/select/select.js
@@ -14,7 +14,7 @@ export function ControllableStates(props) {
         onChange={props.onChange}
         sx={{ width: 300 }}
         renderInput={(params) => (
-          <TextField {...params} label="Selecione a Moeda" />
+          <TextField {...params} label="Selecione a(s) Moeda" />
         )}
       />
     </div>
