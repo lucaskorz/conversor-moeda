9d91566ad071841b706231dd8092d9d6a6fb9a30
Lucas Korz <62682384+lucaskorz@users.noreply.github.com>
2022-04-18 21:56:05 -0300

    Ajustando App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import PageInitial from './components/page-initial/Page-Initial'
-import { createContext, useState } from 'react';
+import { useState } from 'react';
 //import axios from 'axios'
 
 const ThemeContexto = './ThemeContext';
 
 export default function App() {
-  const theme = useState('dark');
-
   return (
     <ThemeContexto>
       <PageInitial/>
